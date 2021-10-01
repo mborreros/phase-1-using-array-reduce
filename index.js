a3ab74eb4a4479c55e57c8d970fb3ff88bfedeb0
Maya Borrero <mlbsindia@gmail.com>
2021-10-01 10:13:07 -0400

    commit tree 8ff3f9ea1c5f8eb10029c837f867b1f05949ecdc

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Create a new variable called totalBatteries, which holds the sum of all of the battery amounts in the batteryBatches array. You should use reduce() for this!
+
+const totalBatteries = batteryBatches.reduce(function (accumulator, batteryElement) {
+    return accumulator + batteryElement;
+})
